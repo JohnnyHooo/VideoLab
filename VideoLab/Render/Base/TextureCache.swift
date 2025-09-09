a1df1837f70edb8059e47590f29ee2d978818cb4
Ziiip <Ziiip>
2025-09-09 12:12:12 +0800

    chore: avoid threading issue and data racing in `TextureCache`

--- VideoLab/Render/Base/TextureCache.swift
+++ VideoLab/Render/Base/TextureCache.swift
@@ -9,35 +9,44 @@
 import Metal
 
 public class TextureCache {
-    var textureCache = [String:[Texture]]()
-    
+    private var textureCache = [String:[Texture]]()
+
+    private let queue = DispatchQueue(label: "com.videolab.texturecache", attributes: .concurrent)
+
     public func requestTexture(pixelFormat: MTLPixelFormat = .bgra8Unorm, width: Int, height: Int) -> Texture? {
         let hash = hashForTexture(pixelFormat: pixelFormat, width: width, height: height)
-        let texture: Texture?
-        
-        if let textureCount = textureCache[hash]?.count, textureCount > 0 {
-            texture = textureCache[hash]!.removeLast()
-        } else {
-            texture = Texture.makeTexture(pixelFormat: pixelFormat, width: width, height: height)
+
+        return queue.sync(flags: .barrier) {
+            if let textures = textureCache[hash], !textures.isEmpty {
+                var mutableTextures = textures
+                let texture = mutableTextures.removeLast()
+                textureCache[hash] = mutableTextures
+                return texture
+            } else {
+                return Texture.makeTexture(pixelFormat: pixelFormat, width: width, height: height)
+            }
         }
-        
-        return texture
     }
-    
-    public func purgeAllTextures() {
-        textureCache.removeAll()
-    }
-    
+
     public func returnToCache(_ texture: Texture) {
         Texture.clearTexture(texture)
         let hash = hashForTexture(pixelFormat: texture.texture.pixelFormat, width: texture.width, height: texture.height)
-        if textureCache[hash] != nil {
-            textureCache[hash]?.append(texture)
-        } else {
-            textureCache[hash] = [texture]
+
+        queue.async(flags: .barrier) {
+            if self.textureCache[hash] != nil {
+                self.textureCache[hash]?.append(texture)
+            } else {
+                self.textureCache[hash] = [texture]
+            }
         }
     }
-    
+
+    public func purgeAllTextures() {
+        queue.async(flags: .barrier) {
+            self.textureCache.removeAll()
+        }
+    }
+
     private func hashForTexture(pixelFormat: MTLPixelFormat = .bgra8Unorm, width: Int, height: Int) -> String {
         return "\(width)x\(height)-\(pixelFormat)"
     }

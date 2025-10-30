b7d72bc6566df218b9bda5aca5d4e1914bf8cfe2
小新小心 <hujin123@vip.qq.com>
2025-10-30 16:37:03 +0800

    fix: Add Swift bridge for BlendMode types to resolve SPM compilation issues

---
+++ VideoLab/Render/Operations/BlendModeSwift.swift
@@ -0,0 +1,15 @@
+//
+//  BlendModeSwift.swift
+//  VideoLab
+//
+//  Created by VideoLab on 2025/10/30.
+//
+
+import Foundation
+
+// Swift版本的BlendMode定义，替代C头文件中的定义
+public typealias BlendMode = Int32
+
+public let BlendModeNormal: BlendMode = 0
+public let BlendModeDarken: BlendMode = 1
+public let BlendModeMultiply: BlendMode = 2
